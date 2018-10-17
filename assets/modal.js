cc324bd4808c6249f7550ab78f609cac4946c096
mustafa8735 <44214441+mustafa8735@users.noreply.github.com>
2018-10-17 10:36:12 +0300

    Add files via upload

--- assets/modal.js
+++ assets/modal.js
@@ -4,156 +4,201 @@
 * this is mostly just to show an example of the things you can
 * do with the Hootsuite SDK.
 */
-'use strict'; 
+'use strict';
 
 function CreateIssue() {
 
     console.log("413123123131231");
+
+    var url = "https://api.mi4biz.net/services/IssueService/478/Issue.json/1";
+    var header = { 'Content-Type': 'application/json', 'Authorization': 'Basic YWRtaW5AZHJkLmNvbTE6MTIzNDU2QWEt', 'Accept': 'text/html,application/xhtml+json,application/json;q=0.9,*/*;q=0.8' };
+
+    var request = '{ \
+                    "Category": { \
+                        "CategoryID": "19039" \
+                    }, \
+                    "ContactMethod": { \
+                        "ContactMethodID": "6887" \
+                    }, \
+                    "Customer": { \
+                        "CustomerID": "C1094076" \
+                    }, \
+                    "IssueDescription": "hoot suite test bildirim", \
+                        "IssueType": { \
+                        "IssueTypeID": "1181" \
+                    }, \
+                    "PreferredReturnMethods": { \
+                        "ContactMethod": { \
+                            "ContactMethodID": "6887", \
+                                "MailConfigurationID": "0" \
+                        } \
+                    } \
+                }';
+
+
+    $.ajax({
+        type: "POST",
+        headers: header,
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: request,
+        async: false,
+        cache: false,
+        success: function (response) {
+
+            console.log("Success: " + response);
+        },
+        error: function (e) {
+
+            console.log("Error: " + e);
+        }
+    });
 }
 
 function populateMessage(data) {
-  // dumps pretty json into hidden area
-  var prettyJSONData = JSON.stringify(data, null, 4);
-  appendTextToClass('hs_jsonDump', prettyJSONData);
+    // dumps pretty json into hidden area
+    var prettyJSONData = JSON.stringify(data, null, 4);
+    appendTextToClass('hs_jsonDump', prettyJSONData);
 
-  // finds network-specific information
-  var avatarURL = 'https://hootsuite.com/dist/images/logos/hootsuite/owly.png';
-  var displayName = 'Hootsuite';
-  var profileURL = 'https://hootsuite.com/';
-  switch (data.post.network) {
-    case 'FACEBOOK':
-      avatarURL = data.profile.picture;
-      displayName = data.profile.name;
-      profileURL = data.profile.link;
-      break;
-    case 'TWITTER':
-      avatarURL = data.profile.profile_image_url_https;
-      displayName = data.profile.name;
-      profileURL = data.profile.url;
-      break;
-    case 'INSTAGRAM':
-      avatarURL = data.profile.profile_picture;
-      displayName = data.profile.full_name;
-      profileURL = 'https://instagram.com/' + data.post.user.username;
-      break;
-    case 'YOUTUBE':
-      avatarURL = data.profile.avatar_url;
-      displayName = data.profile.name;
-      profileURL = 'https://www.youtube.com/channel/' + data.profile.userid;
-      break;
-  }
+    // finds network-specific information
+    var avatarURL = 'https://hootsuite.com/dist/images/logos/hootsuite/owly.png';
+    var displayName = 'Hootsuite';
+    var profileURL = 'https://hootsuite.com/';
+    switch (data.post.network) {
+        case 'FACEBOOK':
+            avatarURL = data.profile.picture;
+            displayName = data.profile.name;
+            profileURL = data.profile.link;
+            break;
+        case 'TWITTER':
+            avatarURL = data.profile.profile_image_url_https;
+            displayName = data.profile.name;
+            profileURL = data.profile.url;
+            break;
+        case 'INSTAGRAM':
+            avatarURL = data.profile.profile_picture;
+            displayName = data.profile.full_name;
+            profileURL = 'https://instagram.com/' + data.post.user.username;
+            break;
+        case 'YOUTUBE':
+            avatarURL = data.profile.avatar_url;
+            displayName = data.profile.name;
+            profileURL = 'https://www.youtube.com/channel/' + data.profile.userid;
+            break;
+    }
 
-  // displays user's display name and attaches url to username
-  appendTextToClass('hs_userName', displayName);
-  getSingleElementByClassName('hs_userName').setAttribute('href', profileURL);
-  getSingleElementByClassName('hs_avatar').setAttribute('title', data.post.user.username);
+    // displays user's display name and attaches url to username
+    appendTextToClass('hs_userName', displayName);
+    getSingleElementByClassName('hs_userName').setAttribute('href', profileURL);
+    getSingleElementByClassName('hs_avatar').setAttribute('title', data.post.user.username);
 
-  // displays user screen name as hover tooltip
-  var screenName = data.post.user.username;
-  if (screenName === null) {
-    getSingleElementByClassName('hs_userName').removeAttribute('title');
-  }
-  getSingleElementByClassName('hs_userName').setAttribute('title', '@' + screenName);
+    // displays user screen name as hover tooltip
+    var screenName = data.post.user.username;
+    if (screenName === null) {
+        getSingleElementByClassName('hs_userName').removeAttribute('title');
+    }
+    getSingleElementByClassName('hs_userName').setAttribute('title', '@' + screenName);
 
-  // displays timestamp, links to post, shows where something was posted from
-  var timestamp = new Date(data.post.datetime);
-  var timeAgo = prettifyTimeAgo(timestamp);
-  appendTextToClass('hs_postTime', timeAgo);
-  var timeElement = getSingleElementByClassName('hs_postTime');
-  timeElement.setAttribute('href', data.post.href);
-  if (data.post.source !== '') {
-    timeElement.setAttribute('title', timestamp.toLocaleString() + " via " + data.post.source);
-  } else {
-    timeElement.setAttribute('title', timestamp.toLocaleString());
-  }
+    // displays timestamp, links to post, shows where something was posted from
+    var timestamp = new Date(data.post.datetime);
+    var timeAgo = prettifyTimeAgo(timestamp);
+    appendTextToClass('hs_postTime', timeAgo);
+    var timeElement = getSingleElementByClassName('hs_postTime');
+    timeElement.setAttribute('href', data.post.href);
+    if (data.post.source !== '') {
+        timeElement.setAttribute('title', timestamp.toLocaleString() + " via " + data.post.source);
+    } else {
+        timeElement.setAttribute('title', timestamp.toLocaleString());
+    }
 
 
-  // display likes, comments, shares
-  // also possible to link to network-specific likes, comments, and share lists
-  appendTextToClass('hs_likesCount', data.post.counts.likes);
-  appendTextToClass('hs_commentsCount', data.post.counts.replies);
-  appendTextToClass('hs_sharesCount', data.post.counts.shares);
+    // display likes, comments, shares
+    // also possible to link to network-specific likes, comments, and share lists
+    appendTextToClass('hs_likesCount', data.post.counts.likes);
+    appendTextToClass('hs_commentsCount', data.post.counts.replies);
+    appendTextToClass('hs_sharesCount', data.post.counts.shares);
 
-  // displays post body
-  appendTextToClass('hs_postBody', data.post.content.body);
+    // displays post body
+    appendTextToClass('hs_postBody', data.post.content.body);
 
-  // displays avatar
-  getSingleElementByClassName('hs_avatarImage').setAttribute('src', avatarURL);
+    // displays avatar
+    getSingleElementByClassName('hs_avatarImage').setAttribute('src', avatarURL);
 
-  getSingleElementByClassName('hs_avatar').addEventListener('click', function () {
-    // This is a Hootsuite SDK function which displays another modal.
-    // This modal can be populated with any data.
-    hsp.customUserInfo({
-      fullName: data.post.user.username,
-      avatar: avatarURL,
-      profileURL: data.profile.link,
-      userLocation: data.profile.location,
-      bio: data.profile.bio,
-      extra: [
-        {
-          label: "Gender",
-          value: data.profile.gender
-        }
-      ]
+    getSingleElementByClassName('hs_avatar').addEventListener('click', function () {
+        // This is a Hootsuite SDK function which displays another modal.
+        // This modal can be populated with any data.
+        hsp.customUserInfo({
+            fullName: data.post.user.username,
+            avatar: avatarURL,
+            profileURL: data.profile.link,
+            userLocation: data.profile.location,
+            bio: data.profile.bio,
+            extra: [
+                {
+                    label: "Gender",
+                    value: data.profile.gender
+                }
+            ]
+        });
     });
-  });
 }
 
 function startEventListeners() {
-  getSingleElementByClassName('hs_showJsonButton').addEventListener('click', function () {
-    if (getSingleElementByClassName('hs_jsonDump').style.display === 'none') {
-      replaceInnerInClass('hs_showJsonButton', 'Hide JSON Payload');
-      getSingleElementByClassName('hs_jsonDump').style.display = 'block';
-      getSingleElementByClassName('hs_copyJsonButton').style.display = 'inline';
-    } else {
-      replaceInnerInClass('hs_showJsonButton', 'Show JSON Payload');
-      getSingleElementByClassName('hs_jsonDump').style.display = 'none';
-      getSingleElementByClassName('hs_copyJsonButton').style.display = 'none';
-    }
-  });
+    getSingleElementByClassName('hs_showJsonButton').addEventListener('click', function () {
+        if (getSingleElementByClassName('hs_jsonDump').style.display === 'none') {
+            replaceInnerInClass('hs_showJsonButton', 'Hide JSON Payload');
+            getSingleElementByClassName('hs_jsonDump').style.display = 'block';
+            getSingleElementByClassName('hs_copyJsonButton').style.display = 'inline';
+        } else {
+            replaceInnerInClass('hs_showJsonButton', 'Show JSON Payload');
+            getSingleElementByClassName('hs_jsonDump').style.display = 'none';
+            getSingleElementByClassName('hs_copyJsonButton').style.display = 'none';
+        }
+    });
 
-  getSingleElementByClassName('hs_copyJsonButton').addEventListener('click', function () {
-    var range = document.createRange();
-    range.selectNode(getSingleElementByClassName('hs_jsonDump'));
-    window.getSelection().empty();
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().empty();
-    replaceTextInClass('hs_copyJsonButton', 'Copied!');
-  });
+    getSingleElementByClassName('hs_copyJsonButton').addEventListener('click', function () {
+        var range = document.createRange();
+        range.selectNode(getSingleElementByClassName('hs_jsonDump'));
+        window.getSelection().empty();
+        window.getSelection().addRange(range);
+        document.execCommand('copy');
+        window.getSelection().empty();
+        replaceTextInClass('hs_copyJsonButton', 'Copied!');
+    });
 
-  getSingleElementByClassName('hs_showGeolocation').addEventListener('click', function () {
-    getGeolocation();
-  });
+    getSingleElementByClassName('hs_showGeolocation').addEventListener('click', function () {
+        getGeolocation();
+    });
 
-  // sets up dropdown menu
-  var topBarControl = getSingleElementByClassName('hs_topBarControlsBtn');
-  topBarControl.addEventListener('click', function (event) {
-    event.preventDefault();
-    if (getSingleElementByClassName('hs_dropdownMenuList').style.display === 'none') {
-      topBarControl.classList.add('active');
-      getSingleElementByClassName('hs_dropdownMenuList').style.display = 'block';
-    } else {
-      topBarControl.classList.remove('active');
-      getSingleElementByClassName('hs_dropdownMenuList').style.display = 'none';
-    }
-  });
+    // sets up dropdown menu
+    var topBarControl = getSingleElementByClassName('hs_topBarControlsBtn');
+    topBarControl.addEventListener('click', function (event) {
+        event.preventDefault();
+        if (getSingleElementByClassName('hs_dropdownMenuList').style.display === 'none') {
+            topBarControl.classList.add('active');
+            getSingleElementByClassName('hs_dropdownMenuList').style.display = 'block';
+        } else {
+            topBarControl.classList.remove('active');
+            getSingleElementByClassName('hs_dropdownMenuList').style.display = 'none';
+        }
+    });
 
-  getSingleElementByClassName('hs_logout').addEventListener('click', function () {
-    window.localStorage.loggedIn = 'false';
-  });
+    getSingleElementByClassName('hs_logout').addEventListener('click', function () {
+        window.localStorage.loggedIn = 'false';
+    });
 
-  getSingleElementByClassName('hs_logout').addEventListener('click', signOut);
+    getSingleElementByClassName('hs_logout').addEventListener('click', signOut);
 }
 
 // for our purposes this is the same thing as jQuery's  $(document).ready(...)
 document.addEventListener('DOMContentLoaded', function () {
-  var data = JSON.parse(window.localStorage.jsonData);
-  //currently handling displaying http errors this way but it can be improved on
-  if ("error" in data) {
-    getSingleElementByClassName('error').style.display = 'block';
-  }
-  populateMessage(data);
-  startEventListeners();
-  googleAuthInit();
+    var data = JSON.parse(window.localStorage.jsonData);
+    //currently handling displaying http errors this way but it can be improved on
+    if ("error" in data) {
+        getSingleElementByClassName('error').style.display = 'block';
+    }
+    populateMessage(data);
+    startEventListeners();
+    googleAuthInit();
 });
